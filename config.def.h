8dab2e6429ca7d3eef87c6a2d310926a03c8a29f
zetatez <zetatez@icloud.com>
2022-07-06 08:53:07 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 0;        /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */  // dwm-swallow
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */  // dwm-swallow
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int barheight          = 24;       /* bh = (barheight > drw->fonts->h ) && (barheight < 3 * drw->fonts->h ) ? barheight : drw->fonts->h + 2 */ // dwm-bar-height
